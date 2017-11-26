7d0420f4bf47a2994226c675ba2a3e7b5ce693d5
w2u2u <warunyu.pon@gmail.com>
2017-11-26 19:50:48 +0700

    Checking should modal will update.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-  <Aux>
-    <Backdrop
-      show={props.show}
-      toggle={props.purchasingToggle}
-    />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0',
-      }}
-    >
-      {props.children}
-    </div>
-  </Aux>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.show !== this.props.show;
+  }
 
-modal.propTypes = {
+  render() {
+    return (
+      <Aux>
+        <Backdrop
+          show={this.props.show}
+          toggle={this.props.purchasingToggle}
+        />
+        <div
+          className={classes.Modal}
+          style={{
+        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: this.props.show ? '1' : '0',
+      }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+Modal.propTypes = {
   children: PropTypes.element.isRequired,
   show: PropTypes.bool.isRequired,
   purchasingToggle: PropTypes.func.isRequired,
 };
 
-export default modal;
+export default Modal;
