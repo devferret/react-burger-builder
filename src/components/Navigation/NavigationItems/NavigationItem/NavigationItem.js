77b504913bea31e5c4460423cdc665b4fa386496
w2u2u <warunyu.pon@gmail.com>
2017-11-26 15:29:40 +0700

    Adding reusable navigation items.

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './NavigationItem.css';
+
+const navigationItem = props => (
+  <li className={classes.NavigationItem} >
+    <a
+      className={props.active ? classes.active : null}
+      href={props.link}
+    >
+      {props.children}
+    </a>
+  </li>
+);
+
+navigationItem.propTypes = {
+  link: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+export default navigationItem;
